4602bdedf2e32f7958932b811dcf846ab30554cf
SanjeevaKumarGeejula <sanjeeva.geejula@rampgroup.com>
2024-12-04 15:35:48 +0530

    sikuli python

---
+++ sikuli/Teams.bat
@@ -0,0 +1,4 @@
+@echo off
+rem Run the .robot file from a specific directory
+robot "C:\Users\SanjeevaKumarGeejula\Downloads\Teams1\Teams\Open_teams.robot"
+exit
---
+++ sikuli/joinTeams.bat
@@ -0,0 +1,4 @@
+@echo off
+rem Run the .robot file from a specific directory
+robot "C:\Users\SanjeevaKumarGeejula\Downloads\Teams1\Teams\Join_Teams.robot"
+exit
